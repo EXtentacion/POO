fd050e7b3f3a639cc64cbb74903b261d3b1625c9
EXtentacion <125932096+EXtentacion@users.noreply.github.com>
2023-02-24 18:35:27 -0600

    Definimos geters y seters , atributos privados

--- Personaje.py
+++ Personaje.py
@@ -1,27 +1,51 @@
 class Personaje:
    
    def __init__(self,esp,nom,alt):
-      self.especie = esp
-      self.nombre = nom
-      self.altura = alt
+      self.__especie = esp
+      self.__nombre = nom
+      self.__altura = alt
          
       
     
    def correr(self,estado):
      if estado == True:
-        print('El personaje'+self.nombre+'esta corriendo')
+        print('El personaje'+self.__nombre+'esta corriendo')
      else:
-        print('El personaje'+self.nombre+'esta quieto')
+        print('El personaje'+self.__nombre+'esta quieto')
         
             
         
    def lanzarGranada(self):
-      print('El personaje'+self.nombre+'lanzo una granada')
+      print('El personaje'+self.__nombre+'lanzo una granada')
 
    def recargarArma(self,municiones):
      cargador=5
      cargador = cargador + municiones
-     print('El personaje'+self.nombre+'recargo su arma y ahora tiene'+str(cargador)+'municiones')
+     print('El personaje'+self.__nombre+'recargo su arma y ahora tiene'+str(cargador)+'municiones')
+     
+     
+   #ejemplo de metodo privado
+   
+   def __pensar(self):
+      print('El personaje'+self.__nombre+'esta pensando')
+      
+   def getNombre(self):
+      return self.__nombre
+   
+   def getEspecie(self):
+      return self.__especie
+   
+   def getAltura(self):
+      return self.__altura
+   
+   def setNombre(self,nombre):
+      self.__nombre = nombre
+      
+   def setEspecie(self,especie):
+      self.__especie = especie
+   
+   def setAltura(self,altura):
+      self.__altura = altura
 
     
     
