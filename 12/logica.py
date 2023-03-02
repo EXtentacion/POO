c2f370eac68a0702ad16cbf3440ecbe57a9c24a4
EXtentacion <125932096+EXtentacion@users.noreply.github.com>
2023-03-01 22:01:57 -0600

    Login

---
+++ 12/logica.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+import tkinter.messagebox as messagebox
+
+class LoginUI:
+    def __init__(self, parent):
+        self.parent = parent
+        self.parent.title("Login")
+        self.parent.geometry("400x200")
+        self.parent.resizable(False, False)
+        self.parent.config(bg="#fff")
+        
+        self.label1 = tk.Label(self.parent, text="Correo", bg="#fff")
+        self.label1.grid(row=0, column=0, padx=10, pady=10)
+        
+        # escribir la contraseña con asteriscos
+        self.label2 = tk.Label(self.parent, text="contraseña", bg="#fff")
+        self.label2.grid(row=1, column=0, padx=10, pady=10)
+          
+        self.entry1 = tk.Entry(self.parent)
+        self.entry1.grid(row=0, column=1, padx=10, pady=10)
+
+        self.entry2 = tk.Entry(self.parent, show="*")
+        self.entry2.grid(row=1, column=1, padx=10, pady=10)
+
+        self.boton1 = tk.Button(self.parent, text="Ingresar", command=self.ingresar)
+        self.boton1.grid(row=2, column=0, padx=10, pady=10)
+
+    def ingresar(self):
+        correo = self.entry1.get()
+        contraseña = self.entry2.get()
+        if  correo ==  'fercho@gmail.com'  and contraseña ==  "12345" :
+            messagebox.showinfo("Login", "Bienvenido")
+        else :
+            messagebox.showerror("Login", "Usuario o contraseña son incorrectos")
+ 
+
+
+
+            
---
+++ 12/once.py
@@ -0,0 +1,10 @@
+from logica import LoginUI
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    app = LoginUI(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
